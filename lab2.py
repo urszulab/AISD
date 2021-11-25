62e1a6936e7152eaebd63e09cba9ff9526a1f627
urszulab <77877386+urszulab@users.noreply.github.com>
2021-11-25 18:32:58 +0100

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -1,10 +1,11 @@
 from typing import Any
 
+
 class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
-        
+
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -12,7 +13,7 @@ class LinkedList:
 
     def push(self, value: Any) -> None:
         if self.head is None:
-            newNode = Node(value)  
+            newNode = Node(value)
             self.head = newNode
             self.tail = newNode
         else:
@@ -20,17 +21,17 @@ class LinkedList:
             newNode.next = self.head
             self.head = newNode
 
-    def append(self, value: Any) -> None: 
+    def append(self, value: Any) -> None:
         if self.head is None:
-            newNode = Node(value)  
+            newNode = Node(value)
             self.head = newNode
             self.tail = newNode
         # mozna tez dac self.push(value)
         else:
             node = self.tail
             newNode = Node(value)
-            node.next = newNode  
-            self.tail = newNode 
+            node.next = newNode
+            self.tail = newNode
 
     def node(self, at: int) -> Node:
         if at < len(self):
@@ -41,7 +42,7 @@ class LinkedList:
         else:
             raise ValueError("There was given an index out of the list.")
 
-    def insert(self, value: Any, after: Node) -> None: 
+    def insert(self, value: Any, after: Node) -> None:
         if after is None:
             print("Given node doesn't exist")
             return
@@ -52,7 +53,7 @@ class LinkedList:
         newNode.next = after.next
         after.next = newNode
 
-    def pop(self) -> Any: 
+    def pop(self) -> Any:
         if len(self) == 0:
             raise ValueError("The list is empty.")
         else:
@@ -90,15 +91,15 @@ class LinkedList:
             after.next = deletedNode.next
             return deletedNode
 
-    def __str__(self) -> str: 
+    def __str__(self) -> str:
         if self.head is None:
             return "The list is empty"
         node = self.head
         resultStr = ""
         while node.next:
-            resultStr += str(node.value) + " -> "
+            resultStr += f'{node.value}' + " -> "
             node = node.next
-        resultStr += str(node.value)
+        resultStr += f'{node.value}'
         return resultStr
 
     def __len__(self) -> int:
@@ -109,25 +110,26 @@ class LinkedList:
             node = node.next
         return amount
 
+
 list_ = LinkedList()
 list_.push(1)
 list_.push(0)
-print(f'The list after usage of the  method .push():  {list_}') 
+print(f'The list after usage of the  method .push():  {list_}')
 
 list_.append(9)
 list_.append(10)
 print(f'The list after usage of the method .append():  {list_}')
 
-print(f'Node behind index 2 is: {list_.node(2).value}')  
+print(f'Node behind index 2 is: {list_.node(2).value}')
 
-list_.insert(12, list_.node(3))  
+list_.insert(12, list_.node(3))
 list_.insert(15, list_.node(2))
 print(f'The list after usage of the method .node():  {list_}')
 
-print(f'Deleted element by method pop() is: {list_.pop()}') 
-print(f'The list after usage of the method .pop():  {list_}') 
+print(f'Deleted element by method pop() is: {list_.pop()}')
+print(f'The list after usage of the method .pop():  {list_}')
 
-print(f'Deleted element by method .remove_lat() is: {list_.remove_last()}') 
+print(f'Deleted element by method .remove_last() is: {list_.remove_last().value}')
 print(f'The list after usage of the method .remove_last():  {list_}')
 
 print(f'The amount of list\'s elements is: {len(list_)}')
@@ -135,11 +137,12 @@ print(f'The amount of list\'s elements is: {len(list_)}')
 print(f'Deleted element after indicated position  is: {list_.remove(list_.node(1)).value}')
 print(f'The list after usage of the method .remove():  {list_}')
 
+
 class Stack:
     def __init__(self):
-       self._storage = LinkedList()
+        self._storage = LinkedList()
 
-    def push(self, element: Any) -> None: 
+    def push(self, element: Any) -> None:
         self._storage.push(element)
 
     def pop(self) -> Any:
@@ -151,11 +154,12 @@ class Stack:
     def __str__(self) -> str:
         resultStr = ""
         for j in range(len(self._storage)):
-          resultStr += f'{(self._storage.node(j).value)} \n'
+            resultStr += f'{(self._storage.node(j).value)} \n'
         return f'Stack looks in this way:\n{resultStr}'
 
     def __len__(self) -> int:
         return len(self._storage)
+
 
 stack = Stack()
 stack.push(21)
@@ -203,6 +207,10 @@ queue.enqueue('Odpowiedz A')
 queue.enqueue('Odpowiedz B')
 queue.enqueue('Odpowiedz C')
 print(queue)
+
+queue.dequeue()
+print(queue)
+print(f'The amount of queue\'s elements is: {len(queue)}')
 
 queue.dequeue()
 print(queue)
